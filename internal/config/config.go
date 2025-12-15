195dfa2833e472a4d73a90c28c7fb7f59328b839
anshika <anshika@vahanwire.com>
2025-12-15 17:15:16 +0530

    config file chng

--- internal/config/config.go
+++ internal/config/config.go
@@ -11,9 +11,11 @@ type Config struct {
 
 func Load() Config {
 	return Config{
-		MongoURI:    getEnv("MONGO_URI", "mongodb://localhost:27017"),
-		MongoDBName: getEnv("MONGO_DB", "provider_db"),
-		RabbitURL:   getEnv("RABBIT_URL", "amqp://guest:guest@localhost:5672/"),
+		// MongoURI:    getEnv("MONGO_URI", "mongodb://localhost:27017"),
+		// MongoDBName: getEnv("MONGO_DB", "provider_db"),
+		MongoURI:  getEnv("MONGO_URI", "mongodb+srv://apps_db_user:PbCklZTgDHXzHUMk@cluster0.3yf1adk.mongodb.net/?appName=Cluster0"),
+		MongoDBName:    getEnv("DB_NAME", "test"),
+		// RabbitURL:   getEnv("RABBIT_URL", "amqp://guest:guest@localhost:5672/"),
 		HTTPAddr:    getEnv("HTTP_ADDR", ":8080"),
 	}
 }
