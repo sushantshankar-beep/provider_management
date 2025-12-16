916d580736b4002f9843bab284e56e9a7a710ee8
sushant shankar <sushant.shankar@vahanwire.com>
2025-12-16 18:12:42 +0530

    changes done for config

--- internal/config/config.go
+++ internal/config/config.go
@@ -10,12 +10,25 @@ type Config struct {
 }
 
 func Load() Config {
-	port := getEnv("PORT", "8080")
+	port := os.Getenv("PORT")
+	if port == "" {
+		httpAddr := os.Getenv("HTTP_ADDR")
+		if httpAddr != "" {
+			return Config{
+				MongoURI:    getEnv("MONGO_URI", "mongodb://localhost:27017"),
+				MongoDBName: getEnv("MONGO_DB", "provider_db"),
+				RabbitURL:   getEnv("RABBIT_URL", "amqp://guest:guest@localhost:5672/"),
+				HTTPAddr:    httpAddr,
+			}
+		}
+		port = "8080"
+	}
 
 	return Config{
-		MongoURI:    getEnv("MONGO_URI", ""),
-		MongoDBName: getEnv("MONGO_DB", "test"),
-		RabbitURL:   getEnv("RABBIT_URL", ""),
+		MongoURI:    getEnv("MONGO_URI", "mongodb://localhost:27017"),
+		MongoDBName: getEnv("MONGO_DB", "provider_db"),
+		RabbitURL:   getEnv("RABBIT_URL", "amqp://guest:guest@localhost:5672/"),
+		HTTPAddr:    getEnv("HTTP_ADDR", ":8080"),
 		HTTPAddr:    ":" + port,
 	}
 }
