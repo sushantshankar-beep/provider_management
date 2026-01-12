5b7698f949e19b39cd17519233c3513db20ac1cb
sushant shankar <sushant.shankar@vahanwire.com>
2026-01-12 12:04:33 +0530

    added changes for dashboard efficiency

--- internal/service/dashboard_service.go
+++ internal/service/dashboard_service.go
@@ -2,11 +2,12 @@ package service
 
 import (
 	"context"
+	"sync"
+
 	"provider_management/internal/dto"
 	"provider_management/internal/repository"
-     "provider_management/internal/utils"
+	"provider_management/internal/utils"
 )
-
 type DashboardService struct {
 	providers    *repository.ProviderRepo
 	users        *repository.UserRepo
@@ -40,80 +41,155 @@ func NewDashboardService(
 	}
 }
 
-func (s *DashboardService) GetDashboardStats(ctx context.Context, period string, days string) (*dto.DashboardResponse, error) {
-    
-	providers, err := s.providers.GetStats(ctx)
+func (s *DashboardService) GetDashboardStats(ctx context.Context,period string,days string) (*dto.DashboardResponse, error) {
+
+	var (
+		providers        dto.ProvidersStats
+		users            dto.UsersStats
+		biddings         dto.BiddingStats
+		bookings         dto.BookingsStats
+		transactionStats dto.TransactionStats
+		settlementStats  dto.SettlementStats
+		complaints       dto.ComplaintsStats
+		revenue          dto.RevenueStats
+		topServices      []dto.TopService
+		amcCount         int64
+	)
+
+	var (
+		wg  sync.WaitGroup
+		err error
+		mu  sync.Mutex
+	)
+
+	setErr := func(e error) {
+		if e != nil {
+			mu.Lock()
+			if err == nil {
+				err = e
+			}
+			mu.Unlock()
+		}
+	}
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		var e error
+		providers, e = s.providers.GetStats(ctx)
+		setErr(e)
+	}()
+
+	// Users
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		var e error
+		users, e = s.users.GetStats(ctx)
+		setErr(e)
+	}()
+
+	// AMC
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		var e error
+		amcCount, e = s.amcPurchases.CountActive(ctx)
+		setErr(e)
+	}()
+
+	// Biddings
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		var e error
+		biddings, e = s.bids.GetBiddingStats(ctx)
+		setErr(e)
+	}()
+
+	// Bookings
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		var e error
+		bookings, e = s.services.GetBookingStats(ctx)
+		setErr(e)
+	}()
+
+	// Transactions
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		var e error
+		transactionStats, e = s.transactions.GetTransactionStats(ctx, days)
+		setErr(e)
+	}()
+
+	// Settlements
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		var e error
+		settlementStats, e = s.settlements.GetStats(ctx, days)
+		setErr(e)
+	}()
+
+	// Complaints
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		var e error
+		complaints, e = s.complaints.GetDashboardStats(ctx)
+		setErr(e)
+	}()
+
+	// Revenue
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		var e error
+		revenue, e = s.transactions.GetAMCRevenueStats(ctx, period)
+		setErr(e)
+	}()
+
+	// Top Services
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		var e error
+		topServices, e = s.services.GetTopServices(ctx)
+		setErr(e)
+	}()
+
+	wg.Wait()
 	if err != nil {
 		return nil, err
 	}
 
-	users, err := s.users.GetStats(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	amcCount, err := s.amcPurchases.CountActive(ctx)
-	if err != nil {
-		return nil, err
-	}
 	users.AMC = amcCount
 
-	biddings, err := s.bids.GetBiddingStats(ctx)
-	if err != nil {
-		return nil, err
+	platformRevenue := utils.RoundTo2(
+		transactionStats.TotalAmount - settlementStats.SettledAmount,
+	)
+
+	baseAmount := utils.RoundTo2(
+		transactionStats.TotalAmount - transactionStats.GSTAmount,
+	)
+
+	var settledPercentage, platformPercentage, gstPercentage float64
+	if baseAmount > 0 {
+		settledPercentage = utils.RoundTo2((settlementStats.SettledAmount / baseAmount) * 100)
+		platformPercentage = utils.RoundTo2((platformRevenue / baseAmount) * 100)
+		gstPercentage = utils.RoundTo2((transactionStats.GSTAmount / baseAmount) * 100)
 	}
 
-	bookings, err := s.services.GetBookingStats(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	transactionStats, err := s.transactions.GetTransactionStats(ctx, days)
-    if err != nil {
-        return nil, err
-    }
-
-	settlementStats, err := s.settlements.GetStats(ctx, days)
-    if err != nil {
-        return nil, err
-    }
-	platformRevenue := utils.RoundTo2(transactionStats.TotalAmount - settlementStats.SettledAmount)
-	baseAmount := utils.RoundTo2(transactionStats.TotalAmount - transactionStats.GSTAmount)
-    
-    var settledPercentage, platformPercentage, gstPercentage float64
-    
-    if baseAmount > 0 {
-        settledPercentage = utils.RoundTo2((settlementStats.SettledAmount / baseAmount) * 100)
-        platformPercentage = utils.RoundTo2((platformRevenue / baseAmount) * 100)
-        gstPercentage = utils.RoundTo2((transactionStats.GSTAmount / baseAmount) * 100)
-    } else {
-        settledPercentage = 0
-        platformPercentage = 0
-        gstPercentage = 0
-    }
 	combinedSettlement := dto.CombinedSettlementStats{
-        TransactionStats: transactionStats,
-        SettlementStats:  settlementStats,
-        PlatformRevenue:  platformRevenue,
-		SettledPercentage: settledPercentage,
-        PlatformPercentage: platformPercentage,
-        GSTPercentage: gstPercentage,
-    }
-    
-
-	complaints, err := s.complaints.GetDashboardStats(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	revenue, err := s.transactions.GetAMCRevenueStats(ctx, period)
-	if err != nil {
-		return nil, err
-	}
-
-	topServices, err := s.services.GetTopServices(ctx)
-	if err != nil {
-		return nil, err
+		TransactionStats:   transactionStats,
+		SettlementStats:    settlementStats,
+		PlatformRevenue:    platformRevenue,
+		SettledPercentage:  settledPercentage,
+		PlatformPercentage: platformPercentage,
+		GSTPercentage:      gstPercentage,
 	}
 
 	return &dto.DashboardResponse{
@@ -121,7 +197,7 @@ func (s *DashboardService) GetDashboardStats(ctx context.Context, period string,
 		Users:       users,
 		Biddings:    biddings,
 		Bookings:    bookings,
-		Settlement:  combinedSettlement, 
+		Settlement:  combinedSettlement,
 		Complaints:  complaints,
 		Revenue:     revenue,
 		TopServices: topServices,
